57f332c5383e78683f104b453a0ff48643bb2225
Amrik Malhans <amrikdeveloper@gmail.comm>
2021-10-09 12:27:10 +0530

    setup server and add handlers

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"github.com/amrikmalhans/go-microservices/handlers"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
-func main()  {
-	fmt.Println("Hello World")
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	s := &http.Server{
+		Addr:         ":8000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Recieved terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	s.Shutdown(tc)
 }
